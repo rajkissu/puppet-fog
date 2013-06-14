cd99883afd1ec7164cfe10c13ae84c5597b4f7c0
Raj Kissu <rajkissu@gmail.com>
2013-06-14 08:28:02 +0000

    Fixes nokogiri dependency at v1.5.9

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,18 @@
 class fog {
-    package { 'fog-dep':
+    package { "fog-dep":
         name   => [ "libxslt-dev", "libxml2-dev" ],
         ensure => present,
+    }
+
+    package { "nokogiri":
+        ensure   => "1.5.9",
+        provider => gem,
     }
 
     package { "fog":
         ensure   => "1.11.1",
         provider => gem,
-        require  => Package['fog-dep'],
+        require  => Package["fog-dep", "nokogiri"],
     }
 
     package { "net-scp":
